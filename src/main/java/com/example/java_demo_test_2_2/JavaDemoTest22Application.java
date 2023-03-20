1ce659b8c7b56ab2d72c8c155ffa73c53016ced4
peishan-Chiang <peishanchiang0816@gmail.com>
2023-03-20 11:44:48 +0800

    the first commit _with test_2_2

---
+++ src/main/java/com/example/java_demo_test_2_2/JavaDemoTest22Application.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test_2_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTest22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTest22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_demo_test_2_2/JavaDemoTest22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test_2_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTest22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
